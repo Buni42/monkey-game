dae3f9bbeb0d5507b846a78b7b87bcf0f883ef5d
Buni42 <Bunyaminanil88@gmail.com>
2023-07-12 22:52:32 +0200

    add files

--- scripts/main/rotatingcam.gd
+++ scripts/main/rotatingcam.gd
@@ -4,7 +4,7 @@ var torque = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("ahh")
+	print("ahhhhhhhhhhh")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
